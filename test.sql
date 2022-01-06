789716c4c368b593e89a132e1e1cfdbf0902f683
Ankush <65169513+Anku-Kashyap@users.noreply.github.com>
2022-01-06 21:33:17 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE studentData(Name varchar(20), Roll_No integer, Address varchar(40), Mobile_Number bigint primary key NOT NULL, PAN_Number varchar(10));
+
+INSERT INTO studentData values('Ankush', 20112011, 'Chandigarh', 8284985573, 'PAN12345');
+INSERT INTO studentData values('Tom', 20112000, 'luckhnow', 12345667, 'PAN12345');
+INSERT INTO studentData VALUES('Lassan', 20112009, 'MUMBAI', 098763234, 'PAN12345');
